79f218464fd0bc0a8d4d39b7b9c100cccbf7f1a4
Oni-olufemi Ayobami <aycodergymer21@gmail.com>
2022-11-15 08:20:15 +0100

    Added my resume

--- src/components/header/Cta.jsx
+++ src/components/header/Cta.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import finishing_strong from '../../assets/finishing_strong.pdf'
+import Ayobami from '../../assets/Ayobami.docx'
+
 
 function Cta() {
   return (
       <div className='cta'>
-          <a href={finishing_strong} download className="btn cta1"> Download CV</a>
+          <a href={Ayobami} download className="btn cta1"> Download CV</a>
           <a href="#contact" className='btn btn-primary cta2'>Let's talk</a>
     </div>
   )
